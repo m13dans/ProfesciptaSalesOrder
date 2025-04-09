aee65a5f2e3b3fd3f13dfc5be12a1951ff535f7a
danur <danuraryapratama@gmail.com>
2025-04-09 12:28:38 +0700

    add create page

--- ProfesciptaSalesOrder.Web/Services/SalesOrderService.cs
+++ ProfesciptaSalesOrder.Web/Services/SalesOrderService.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using ProfesciptaSalesOrder.Web.Data;
 using ProfesciptaSalesOrder.Web.Models;
+using System.Text.Json.Serialization;
 
 namespace ProfesciptaSalesOrder.Web.Services;
 
@@ -8,6 +9,8 @@ namespace ProfesciptaSalesOrder.Web.Services;
 public class SalesOrderRequest
 {
     public string? Keywords { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public DateTime? OrderDate { get; set; }
 }
 
@@ -53,15 +56,16 @@ public class SalesOrderService(ConnectionString connectionString)
             SELECT TOP 100 so.SO_ORDER_ID SalesOrderId, so.ORDER_NO OrderNo, so.ORDER_DATE OrderDate, cust.CUSTOMER_NAME CustomerName
             FROM SO_ORDER so
             JOIN COM_CUSTOMER cust ON so.COM_CUSTOMER_ID = cust.COM_CUSTOMER_ID
-            WHERE so.SO_ORDER_ID LIKE '%' + @Keywords + '%'
-            OR cust.CUSTOMER_NAME LIKE '%' + @Keywords + '%'
-            AND so.ORDER_DATE = @OrderDate
+            WHERE (so.ORDER_NO LIKE '%' + @Keywords + '%'
+            OR cust.CUSTOMER_NAME LIKE '%' + @Keywords + '%')
+            AND (@OrderDate IS NULL OR so.ORDER_DATE = @OrderDate)
         ";
 
         try
         {
             using var conn = connectionString.Create();
 
+            request.Keywords ??= "";
             var queryResult = await conn.QueryAsync<SalesOrderResponse>(sql, new { request.Keywords, request.OrderDate });
             var result = queryResult.ToList();
 
